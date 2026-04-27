1d5e056c893999588b1fbb44d6fa89f20c872f1a
Mohamed Tamer <120857321+M-Max555@users.noreply.github.com>
2026-04-28 00:50:10 +0300

    fix gemini model

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash"
+  model: "gemini-1.5-flash-8b"
 });
 
 app.post("/chat", async (req, res) => {
